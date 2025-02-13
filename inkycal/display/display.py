15684243c6ef0baf6ffec64c58a76acd26a6c6e9
Sean Croft <sean.croft@cebrands.ca>
2025-02-13 13:22:20 -0800

    fix arguments

--- inkycal/display/display.py
+++ inkycal/display/display.py
@@ -49,7 +49,7 @@ class Display:
         # TODO implement test image
         raise NotImplementedError("Devs were too lazy again, sorry, please try again later")
 
-    def render(self, im_black: PIL.Image, im_black: PIL.Image or None = None) -> None:
+    def render(self, im_black: PIL.Image, im_colour: PIL.Image or None = None) -> None:
         """Renders an image on the selected E-Paper display.
 
         Initlializes the E-Paper display, sends image data and executes command
